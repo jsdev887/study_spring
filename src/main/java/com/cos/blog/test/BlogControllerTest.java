7171071287fd2766fa2e2694a11458703574fed3
Jiwoo <jsdev887@gmail.com>
2021-07-21 13:55:58 +0900

    Environment setting

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BlogControllerTest {
 	
+	//http://localhost:8080/test/hello
 	@GetMapping("/test/hello")
 	public String hello() {
 		return "<h1>Hello spring boot</h1>";
