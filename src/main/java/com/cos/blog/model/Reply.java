22faf133fd8889a65b4c41d91fb2440a60fb5910
Jiwoo <jsdev887@gmail.com>
2021-07-22 16:21:37 +0900

    create Reply

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,5 +1,43 @@
 package com.cos.blog.model;
 
-public class Reply {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Reply {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 200)
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private Board board;
+	
+	@ManyToOne // 여러개의 답변을 하나의 유저가 달 수 있다. 
+	@JoinColumn(name = "boardId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
