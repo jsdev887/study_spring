2bd82abad58b4afdb071641770b5e0132e6b3299
Jiwoo <jsdev887@gmail.com>
2021-07-22 10:46:56 +0900

    Constructor

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -10,13 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 // 사용자가 요청 -> 응답(HTML 파일)
 //@Controller
 // 사용자가 요청 -> 응답(Data)
-@RestController
+@RestController // 여기서는 String 그 자체를 리턴함 
 public class HttpControllerTest {
+	
+	private static final String TAG = "HttpControllerTest";
+	
+	// http://localhost:8000/blog/http/lombok
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//	Member m = new Member(10, "ssar", "dddd", "");
+		// 빌더 장점 1. 생성자가 틀어질 경우(순서가 바뀌거나 할 경우) 코드가 틀어질 수 있는데 빌더쓰면 알아서 잘 해줌 .
+		Member m = Member.builder().password("builder1234").username("iserrrr").email("ttttt.@n.co,").build();
+
+		System.out.println(TAG+"getter:" + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"setter:" + m.getUsername());		
+		return "lombok test complete"; 
+	}
 	
 	//터넷 브라우저 요청은 무조건 get요청밖에 할 수 없
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest(Member m) {//id=1&username=ssar&password=1234&email=ssar@nate.com
+	public String getTest(Member m) {//id=1&username=ssar&password=1234&email=ssar@nate.com		
 		return "get 요청:"+m.getId()+"," + m.getUsername()+","+m.getPassword()+","+m.getEmail();	
 	}
 	
@@ -27,7 +42,7 @@ public class HttpControllerTest {
 	}
 	
 	//http://localhost:8080/http/put (update)
-	@PutMapping("/http/put")
+	@PutMapping("/http/put") 
 	public String putTest(@RequestBody Member m) {
 		return "put 요청"+m.getId()+"," + m.getUsername()+","+m.getPassword()+","+m.getEmail();	
 	}
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,44 +1,32 @@
 package com.cos.blog.test;
 
-public class Member {
-	
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Data//Getter/Setter 동시에 해준다. 
+//@AllArgsConstructor // 생성자.
+//@RequiredArgsConstructor //  AllArgsConstructor 대신 근데 요즘에는 final 붙여서 해줌 . 예) private final int id;
+//@NoArgsConstructor // 빈생성자 
+
+@Data
+@NoArgsConstructor
+public class Member {	
 	private int id;
 	private String username;
 	private String password;
 	private String email;
 	
-	
+	@Builder 
 	public Member(int id, String username, String password, String email) {
-		this.id = id;
+ 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
 }
