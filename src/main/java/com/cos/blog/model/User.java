b6ec2e3c139f9d30ef24f85194dad21c5b0efdc6
Jiwoo <jsdev887@gmail.com>
2021-07-22 15:24:01 +0900

    create board

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,7 +11,16 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@Builder
 @Entity // User 클래스가 mysql 에 테이블이 생성이 된다.
 public class User {
 	
