2346dcf57446be88b8c8337cc69b42f43c68271d
Jiwoo <jsdev887@gmail.com>
2021-07-26 10:53:35 +0900

    Add test sample

---
+++ src/main/java/com/cos/blog/service/TestService.java
@@ -0,0 +1,10 @@
+package com.cos.blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+	 public String getName(){
+	        return "saelobi";
+	    }
+}
---
+++ src/main/java/com/cos/blog/test/HelloController.java
@@ -0,0 +1,13 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/blog/hello")
+    public String hello(){
+        return "hello";
+    }
+}
