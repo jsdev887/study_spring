ef93e8d685f34e1476560959937d924770a94000
Jiwoo <jsdev887@gmail.com>
2021-07-30 13:32:26 +0900

    add update

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -23,8 +27,35 @@ import com.cos.blog.service.UserService;
 public class DummyControllerTest {
 
 	//얘는 기본적으로 null 근데 @Autowired 를 넣어주면DummyControllerTest가 메모리에 띄워질 때 UserRepository도 같이 띄워진다.
-	@Autowired //의존성 주입(DI)
+	@Autowired //의존성 주입(DI) 
 	private UserRepository userRepository;
+	
+	// save 함수는 id를 전달하지 않으면 insert를 해주고
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+	@Transactional // 해당 어노테이션을 붙이면 save를 하지 않아도 업데이트가 된다. 함수 종료시 자동commit 이 됨.
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // json으로 받으려면 RequestBody가 필요하다.json 데이터를 요청 => java object(MessageConvter의 jackson 라이브러리가 변환해서 받아줌. ) 
+		System.out.println("id: "+id);
+		System.out.println("password: "+requestUser.getPassword());
+		System.out.println("email: "+requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정 실패");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+//		userRepository.save(user);
+		
+// 
+//		requestUser.setId(id);
+//		requestUser.setUsername("ssar");
+//		userRepository.save(requestUser); // save로 할 경우 기존의 id가 있을 때 해당 데이터를 덮어 씌우는데 위의 코드에서 password와 email은 값이 있지만 나머지는(이름의 경우ssar 로 하드코딩) 없으므로 null 값이 들어간다.
+		
+		// @Transactional 로 인해 더티체킹
+		return user;
+	}
 	
 	// 유저 전체를 다 호출하기 때문에 파라미터(id 값 등)을 따로 받을 필요 없음.
 	//http://localhost:8000/blog/dummy/user
