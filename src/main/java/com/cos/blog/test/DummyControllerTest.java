4e77582230847bdfa0268405914ee22ff0b53688
Jiwoo <jsdev887@gmail.com>
2021-08-02 09:33:52 +0900

    add delete

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +35,18 @@ public class DummyControllerTest {
 	// save 함수는 id를 전달하지 않으면 insert를 해주고
 	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
 	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제를 실패하였습니다. id: "+id;
+		}
+		
+		return "삭제되었습니다 id: "+id;
+	}
+	
 	@Transactional // 해당 어노테이션을 붙이면 save를 하지 않아도 업데이트가 된다. 함수 종료시 자동commit 이 됨.
 	@PutMapping("/dummy/user/{id}")
 	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // json으로 받으려면 RequestBody가 필요하다.json 데이터를 요청 => java object(MessageConvter의 jackson 라이브러리가 변환해서 받아줌. ) 
